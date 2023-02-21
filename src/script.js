11ffc10a706da40b7527e050348a758f47f4d8b5
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-21 22:33:56 +0800

    feat: add addBookToLibrary function

--- src/script.js
+++ src/script.js
@@ -1,21 +1,32 @@
-let myLibrary = []
+const myLibrary = [];
 
 function Book(title, author, numPages, haveRead) {
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages;
-    this.haveRead = haveRead;
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.haveRead = haveRead;
 }
 
 Book.prototype.info = function () {
+  let message;
+  if (this.haveRead) {
+    message = "have read already";
+  } else {
+    message = "not read yet";
+  }
 
-    let message;
-    if (this.haveRead) {
-        message = "have read already"
-    }
-    else {
-        message = "not read yet"
-    }
+  console.log(
+    `${this.title} by ${this.author}, ${this.numPages} pages, ${message}`
+  );
+};
 
-    console.log(`${this.title} by ${this.author}, ${this.numPages} pages, ${message}`);
+function addBookToLibrary() {
+  const title = prompt("What is the title of the book?");
+  const author = prompt("Who is the author of the book?");
+  const numPages = prompt("How many pages are in the book?");
+  const haveRead = prompt("Have you read it already? Y/N");
+
+  const toAppend = new Book(title, author, numPages, haveRead);
+
+  myLibrary.push(toAppend);
 }
